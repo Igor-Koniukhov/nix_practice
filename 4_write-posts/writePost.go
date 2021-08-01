15c0e901749271e9b5a5c1b3ade1f54552c6c67a
Igor Koniukhov <ikoniukhov@gmail.com>
2021-08-01 22:03:16 +0300

    fix permision of filepath in writePost.go (golangci-lint recomandation)

--- 4_write-posts/writePost.go
+++ 4_write-posts/writePost.go
@@ -10,14 +10,14 @@ import (
 )
 
 var waitG sync.WaitGroup
-
+var conn *http.Response
 func main() {
 	numbersOfFiles := 10
 	n := &numbersOfFiles
 	filePath := "./storage/posts/"
 	url := "https://jsonplaceholder.typicode.com/posts/"
 
-	_ = os.MkdirAll(filePath, 666)
+	_ = os.MkdirAll(filePath, 0766)
 	conn, err := http.Get(url)
 	if err !=nil {
 		fmt.Println(err)
@@ -44,16 +44,19 @@ func fileCreator(filePath string, n *int) {
 }
 
 func fileWriter(url string, err error, conn *http.Response, filePath string, n *int) {
+
 	for i := 1; i <= *n; i++ {
 		urlComment := fmt.Sprintf("%s%d", url, i)
 		if err !=nil {
 			fmt.Println(err)
 		}
 		conn, err = http.Get(urlComment)
+
+
 		if err !=nil {
 			fmt.Println(err)
 		}
-		fl, err := os.OpenFile(filePath+strconv.Itoa(i)+".txt", os.O_RDWR, 666)
+		fl, err := os.OpenFile(filePath+strconv.Itoa(i)+".txt", os.O_RDWR, 0766)
 		if err !=nil {
 			fmt.Println(err)
 		}
